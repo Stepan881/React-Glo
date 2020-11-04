7b2bb3679078cec6912c2c9843b3f7922a15941c
stepan881 <stepanstepanov180@gmail.com>
2020-11-04 18:16:16 +0300

    header styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import './App.css';
+import React from 'react';
+import {createGlobalStyle} from 'styled-components';
+
+import {NavBar} from './Components/NavBar';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+  body {
+    margin: 0;
+    background-color: #f0f0f0;
+    font-family: Roboto, sans-serif;
+    font-size: 20px;
+    color: black;
+  }
+  a {
+    text-decoration: inherit;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  h1, h2, h3{
+    font-family: Pacifico, sans-serif;
+    padding: 0;
+    margin: 0;
+  }
+  p {
+    padding: 0;
+    margin: 0;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      hello react
-    </div>
+    <React.Fragment>
+
+      <GlobalStyle />
+      <NavBar />
+
+    </React.Fragment>
   );
 }
 
